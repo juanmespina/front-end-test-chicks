99d71757a9946125084433693d6ffad355df49c2
Juan Espina <jmespina1997@gmail.com>
2023-05-13 20:08:12 -0400

    patch: Delete placeholder code

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BaseLayout from 'src/layout/BaseLayout'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BaseLayout></BaseLayout>
   </React.StrictMode>,
 )
